4ac81087a60c3c4242e9c7760014cc072ffeab70
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-03 08:33:35 -0800

    added EP to get a project by id

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,10 +1,23 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model")
+
+async function validateProjectId(req, res, next) {
+  const { id } = req.params
+  const project = await Projects.get(id)
+  if (project) {
+    next()
+  } else {
+    next({ status: 404, message: "Could not find project" })
+  }
+}
+
 function handleError(err, req, res, next) {
   res.status(err.status || 500).json({
-    "message": `${err.message}`
+    message: `${err.message}`
   })
 }
 
 module.exports = {
-  handleError
+  validateProjectId,
+  handleError,
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,16 +2,31 @@
 // ===== IMPORTS =====
 const express = require("express")
 const Projects = require("./projects-model")
-const { handleError } = require("./projects-middleware")
+const {
+  validateProjectId,
+  handleError
+} = require("./projects-middleware")
 
 // ===== INSTANCE OF EXPRESS =====
 const router = express.Router()
 
 // ===== ENDPOINTS =====
+// [GET] /api/projects (returns an array of all the projects)
 router.get("/", async (req, res, next) => {
   try {
     const allProjects = await Projects.get()
     res.status(200).json(allProjects)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// [GET] /api/projects/:id (returns a specific)
+router.get("/:id", validateProjectId, async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const project = await Projects.get(id)
+    res.status(200).json(project)
   } catch (error) {
     next(error)
   }
