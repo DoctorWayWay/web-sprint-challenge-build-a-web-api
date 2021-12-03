979631b1b5b50daf5e8198caa21fa4b9f283393d
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-03 07:23:11 -0800

    initialized project

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const server = express();
+// ===== IMPORTS =====
+require('dotenv').config()
+const express = require('express')
+
+// ===== INSTANCE OF EXPRESS =====
+const server = express()
+
+// ===== MIDDLEWARE =====
+server.use(express.json())
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
-module.exports = server;
+module.exports = server
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// ===== IMPORTS =====
+require("dotenv").config()
+const server = require("./api/server")
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server listening on port ${PORT} ***\n`)
+})
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
